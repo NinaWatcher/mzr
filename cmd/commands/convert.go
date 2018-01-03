29fcdeec4c18230c612dfb530bee5c485cf856aa
NinaWatcher <34721003+NinaWatcher@users.noreply.github.com>
2018-01-03 18:09:36 +0200

    Update convert.go

--- cmd/commands/convert.go
+++ cmd/commands/convert.go
@@ -1,17 +1,16 @@
-package commands
+package cmd
 
 import (
 	"io/ioutil"
-	"log"
 	"fmt"
+	"os"
 	"strings"
+	"errors"
 	"github.com/spf13/cobra"
-	YAML "github.com/ghodss/yaml"
-	"github.com/mitchellh/go-homedir"
+	"github.com/ghodss/yaml"
 )
 
 var target string
-
 var outputDir string
 
 var cmdBuild = &cobra.Command{
@@ -30,24 +29,34 @@ var cmdConvert = &cobra.Command{
 }
 
 func convert(args []string) {
-
-	home, err := homedir.Dir()
-	yamlDir := home + args[0] + "file.yaml"
-
-	//read file
-	yamlFile, err := ioutil.ReadFile(yamlDir)
-	if err != nil {
-		log.Printf("yamlFile.Get err   #%v ", err)
-	}
-	//Format the yaml to json
-	fmt.Println("Start converting: " + strings.Join(args, " "))
-	jsonOut, err := YAML.YAMLToJSON(yamlFile)
-	fmt.Println(string(jsonOut))
-	err = ioutil.WriteFile("jsonFile.json", jsonOut, 0644)
+	 if len(args[0]) > 0{
+		 //read file
+ 		yamlFile, err := ioutil.ReadFile(args[0])
+ 		AbortIfError(err, "failed to read the yaml file from the provided location")
+ 		//Format the yaml to json
+ 		fmt.Println("Start converting: " + strings.Join(args, " "))
+ 		jsonOut, err := yaml.YAMLToJSON(yamlFile)
+ 		AbortIfError(err, "failed to convert YAML to JSON")
+ 		err = ioutil.WriteFile("jsonFile.json", jsonOut, 0644)
+		AbortIfError(err, "failed to write converted JSON to file")
+		fmt.Println("YAML converted to jsonFile.json")
+	 } else {
+		 AbortIfError(errors.New(""), "please provide a valid path to the yml file")
+	 }
 }
 
 func init() {
 	cmdBuild.AddCommand(cmdConvert)
 	rootCmd.AddCommand(cmdBuild)
 	cmdBuild.PersistentFlags().StringVarP(&outputDir, "convert", "o", "", "filesystem path to write files to")
+}
+
+func AbortIfError(err error, msg string) {
+	if err != nil{
+		fmt.Printf("[ERROR] %s", msg)
+		if "dev" == os.Getenv("RUN_MODE"){
+			fmt.Printf("[ERROR] msg: %s reason: %s\n", msg, err.Error())
+		}
+		os.Exit(1)
+	}
 }
